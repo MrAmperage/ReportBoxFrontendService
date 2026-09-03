b9b07c17facae9623dab12038b68f117060a8691
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2026-09-03 13:58:38 +0700

    Добавлен AngularCore

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-docker buildx build --platform linux/amd64 -t mr.amperage/report_box_frontend_service:0.0.1 .
+#!/bin/sh
+
+docker buildx build \
+    --platform linux/amd64 \
+    --load \
+    -t mr.amperage/report_box_frontend_service:0.0.1 \
+    .
---
+++ src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import Shell from './layout/shell/shell';
+
+export const routes: Routes = [{ path: '', component: Shell }];
