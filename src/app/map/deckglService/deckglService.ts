838561d2c5aa0ee2f9f17a774ca86bb222b247ae
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2026-09-08 14:04:15 +0700

    Добавлена регистрация виджета

--- src/app/map/deckglService/deckglService.ts
+++ src/app/map/deckglService/deckglService.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Deck } from 'deck.gl';
 import { BehaviorSubject } from 'rxjs';
+import { BaseWidgetOptions } from './deckglServiceTypes';
 
 @Injectable()
 export default class DeckGlService {
@@ -14,6 +15,14 @@ export default class DeckGlService {
     const OldOption = this.WidgetOptionsMap.get(Id);
     if (OldOption !== undefined) {
       OldOption.next({ ...OldOption.getValue(), ...Option });
+    }
+  }
+
+  RegisterWidget<OptionsType extends BaseWidgetOptions>(Options: OptionsType) {
+    if (this.WidgetOptionsMap.has(Options.Id)) {
+      throw new Error(`Виджет ${Options.Id} уже зарегистрирован`);
+    } else {
+      this.WidgetOptionsMap.set(Options.Id, new BehaviorSubject(Options));
     }
   }
 }
