a7bdfc098b1496163c7bca8bcc55f78a987cc32b
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2026-09-08 13:21:35 +0700

    Добавлен базовый компонент для инициализации Deck.gl

--- src/app/map/deckgl/deckgl.ts
+++ src/app/map/deckgl/deckgl.ts
@@ -1,8 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Deck } from 'deck.gl';
 
 @Component({
   selector: 'deckgl',
   templateUrl: './deckgl.html',
+  styleUrl: './deckgl.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class DeckGl {}
+export default class DeckGl implements OnInit {
+  @ViewChild('Container', { static: true })
+  Container!: ElementRef<HTMLDivElement>;
+  Deck!: Deck;
+
+  InitDeckGl() {
+    this.Deck = new Deck({ parent: this.Container.nativeElement });
+  }
+  ngOnInit(): void {
+    this.InitDeckGl();
+  }
+}
