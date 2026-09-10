6627f24584bd886ef924b824add6acd5460e67a0
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2026-09-10 16:31:57 +0700

    Обновлен компонент базового виджета

--- src/app/map/widgets/baseWidget/baseWidget.ts
+++ src/app/map/widgets/baseWidget/baseWidget.ts
@@ -1,8 +1,19 @@
 import { Widget } from 'deck.gl';
 import DeckGlService from '../../deckglService/deckglService';
-
-export default abstract class BaseWidget extends Widget {
+import { OnInit, Component } from '@angular/core';
+@Component({ selector: 'BaseWidget', template: '' })
+export default abstract class BaseWidget extends Widget implements OnInit {
   constructor(private DeckGlService: DeckGlService) {
     super({});
+  }
+
+  InitWidget() {
+    const Widgets = this.DeckGlService.Deck.props.widgets;
+    Widgets.push(this);
+    this.DeckGlService.Deck.setProps({ widgets: Widgets });
+  }
+
+  ngOnInit(): void {
+    this.InitWidget();
   }
 }
